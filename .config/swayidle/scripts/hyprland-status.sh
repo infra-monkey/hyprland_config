3f21b80cd8fa48786c0ee5fec9d8d55c8e30f282
Antoine Gatineau <antoine.gatineau@infra-monkey.com>
2023-08-17 15:02:14 +0200

    fix: waybar module size and contextual menu

--- .config/swayidle/scripts/hyprland-status.sh
+++ .config/swayidle/scripts/hyprland-status.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-cat > $HOME/.cache/swayidle-workspace <<EOF
+cat > /home/antoine/.cache/swayidle-workspace <<EOF
 ACTIVE_WORKSPACE=$(hyprctl activeworkspace -j | jq '.id')
 ACTIVE_MONITOR=$(hyprctl monitors -j | jq '.[] | select(.focused==true) | .id')
 EOF
