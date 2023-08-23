0837bc3969acfbf99fcb91800e7988a882760040
Antoine Gatineau <antoine.gatineau@infra-monkey.com>
2023-08-23 16:29:16 +0200

    mino fix

--- .config/swayidle/scripts/hyprland-resume.sh
+++ .config/swayidle/scripts/hyprland-resume.sh
@@ -2,10 +2,11 @@
 
 ACTIVE_WORKSPACE=1
 ACTIVE_MONITOR=0
-if [ -f "$HOME/.cache/swayidle-workspace" ] ; then
-    . "$HOME/.cache/swayidle-workspace"
+if [ -f ".cache/swayidle-workspace" ] ; then
+    source ".cache/swayidle-workspace"
 fi
-
+echo "hyprctl dispatch workspace $ACTIVE_WORKSPACE" >> /tmp/resume
 hyprctl dispatch dpms on
+sleep 1
 hyprctl dispatch focusmonitor $ACTIVE_MONITOR
 hyprctl dispatch workspace $ACTIVE_WORKSPACE
