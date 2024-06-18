48b309094d8679d479fba9b3e59e57336e5fc402
Antoine Gatineau <antoine.gatineau@infra-monkey.com>
2024-06-18 15:51:35 +0200

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting
-    fish_add_path -a /usr/local/go/bin
+    fish_add_path -a /home/monkey/.local/go/bin
 end
