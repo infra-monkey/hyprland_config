224a78596ffdacec87f8fc06bd6798a4a70ca7a2
Antoine Gatineau <antoine.gatineau@infra-monkey.com>
2025-02-11 17:14:44 +0100

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting
+    set -U fish_prompt_pwd_dir_length 0
     fish_add_path -a /home/monkey/.local/go/bin
 end
--- .config/hypr/scripts/xdg.sh
+++ .config/hypr/scripts/xdg.sh
@@ -8,6 +8,9 @@
 # ----------------------------------------------------- 
 sleep 1
 
+# stop dbus-kded6 service
+systemctl stop --user dbus-:1.2-org.kde.kded6@0.service
+
 # kill all possible running xdg-desktop-portals
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-gnome
@@ -25,4 +28,3 @@ sleep 2
 # start xdg-desktop-portal
 systemctl --user start xdg-desktop-portal
 sleep 1
-
