1117ba2fc0ac0f9946abd2efbee3cd9ffea4edef
Antoine Gatineau <antoine.gatineau@infra-monkey.com>
2025-05-27 08:58:18 +0200

    config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,6 @@ if status is-interactive
     set -U fish_greeting
     set -U fish_prompt_pwd_dir_length 0
     fish_add_path -a /home/monkey/.local/go/bin
+    fish_add_path -a /home/monkey/.local/bin
     fish_ssh_agent
 end
